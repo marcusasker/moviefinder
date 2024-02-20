765997fde0c31485a60e7baf2e20bde54c41e938
Marcus Asker <asker.marcus@gmail.com>
2024-02-20 18:21:13 +0100

    feat: navbar component and searchbar component

--- moviefinder/src/App.js
+++ moviefinder/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { Searchbar } from './components/Searchbar/Searchbar';
+import { Navbar } from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Searchbar />
     </div>
   );
 }
---
+++ moviefinder/src/components/Searchbar/Searchbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Searchbar = () => (
+  <div>
+    <input type="text" placeholder="Search..." />
+    <button type="button">Search</button>
+  </div>
+)
